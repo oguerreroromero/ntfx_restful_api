44f1d99df7089e25f50504f3fe94fdd0eb95541b
tavorex <tavo_89173@hotmail.com>
2023-08-25 20:00:04 -0500

    main

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log("Puerto : "+process.env.PORT);
+  
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
